175bb14c2eec887d0c1a69816ff660223bc70728
Jake <24jllewis@gmail.com>
2019-02-22 12:30:57 -0800

    Initial Commit to Create BackProp NN

---
+++ NeuralNet.py
@@ -0,0 +1,12 @@
+# Jacob Lewis
+# Creating a back-prop NN in python
+
+import numpy as np
+
+class NeuralNetwork:
+    """
+    TODO: Class Docstring
+    """
+    def __init__(self, X, y):
+        self.X = X
+        self.y = y
