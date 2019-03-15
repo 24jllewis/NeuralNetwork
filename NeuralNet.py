161e84f78069f37b3e903b79d8302591d65b1bbe
jllewis <24jllewis@gmail.com>
2019-03-15 13:18:37 -0700

    NN good stuff

--- NeuralNet.py
+++ NeuralNet.py
@@ -27,7 +27,7 @@ class NeuralNetwork:
 
         # create the filters for
         # hardcoded, needs to be updated
-        self.filter = np.zeros((2, 3, 3))
+        self.filter = np.zeros((1, 3, 3))
         self.filter = np.random.normal(size=(8, 1, 5, 5))
         # self.filter[1, :, :] = np.array([[[1, 1, 1],
         #                              [0, 0, 0],
@@ -53,10 +53,10 @@ class NeuralNetwork:
 
     def train(self):
         losses = []
+        print(len(self.x))
         for index, image in enumerate(self.x):
-            grads, loss = self.conv(image.reshape((1, 8, 8)), self.y[index], self.filter, self.weights[0], self.weights[1])
-
-            print(self.y[index])
+            y = np.eye(10)[int(self.y[index])].reshape(10, 1)  # convert label to one-hot
+            grads, loss = self.conv(image.reshape((1, 8, 8)), y, self.filter, self.weights[0], self.weights[1])
 
             self.filter += grads[0]
             self.weights[0] += grads[1]
@@ -64,31 +64,35 @@ class NeuralNetwork:
 
             losses.append(loss)
 
-        print(losses)
+        print(len(losses))
 
         return np.mean(losses)
 
     def conv(self, image, label, filter, weight1, weight2):
-        conved = self.convolution(image, filter)
-        conved = relu(conved)
+        conved = relu(self.convolution(image, filter))
+        # conved[conved <= 0] = 0
 
         fc = conved.reshape((conved.shape[0] * conved.shape[1] * conved.shape[1], 1))
 
         z = relu(weight1.dot(fc))
+        # z[z <= 0] = 0
 
         probabilities = self.FC(weight2.dot(z))
 
+        # probabilities = weight2.dot(z)
         loss = -np.sum(label * np.log(probabilities))
 
-        dout = probabilities - label
+        dout = probabilities - label            # check in to see if this is correct
         dweight2 = dout.dot(z.T)
-        dz = relu_derivative(weight2.T.dot(dout))
+        dz = weight2.T.dot(dout)
+        dz[z <= 0] = 0
 
         dweight1 = dz.dot(fc.T)
 
         dfc = weight1.T.dot(dz)
 
-        dconved = relu_derivative(dfc.reshape(conved.shape))
+        dconved = dfc.reshape(conved.shape)
+        dconved[conved <= 0] = 0
 
         dimage, dfilter = self.convolutionBackward(dconved, image, filter)
 
@@ -103,7 +107,6 @@ class NeuralNetwork:
         output = np.zeros((filter.shape[0], out_dim, out_dim))
 
         assert image.shape[0] == filter.shape[1]
-
 
         # perform the convolution
         for filter_num in range(filter.shape[0]):
@@ -145,90 +148,74 @@ class NeuralNetwork:
 
 
     def FC(self, X):
-        out = np.exp(X)
+        out = np.exp(X - np.max(X))
         return out / np.sum(out)
 
-    # def conv(self, img, conv_filter):
-    #     feature_maps = np.zeros((img.shape[0] - conv_filter.shape[1] + 1, img.shape[1]-conv_filter.shape[1]+1, conv_filter.shape[0]))
-    #     for filter_num in range(conv_filter.shape[0]):
-    #         curr_filter = conv_filter[filter_num, :]
-    #         conv_map = self.conv_(img[:, :, 0], curr_filter[:, :, 0])
-    #         for ch_num in range(1, curr_filter.shape[-1]):
-    #             conv_map = conv_map + self.conv_(img[:, :, ch_num],
-    #                                         curr_filter[:, :, ch_num])
-    #     feature_maps[:, :, filter_num] = conv_map
+    # def feedforward(self):
+    #     """
+    #     Performs the feedforward action in the NN
+    #     :return:
+    #     """
+    #     self.layers = []
+    #     # create the first layer using the NN input x
+    #     layer = relu(np.dot(self.x, self.weights[0]))
+    #     self.layers.append(layer)
     #
-    # def conv_(self, img, conv_filter):
-    #     result = np.zeros(img.shape)
-    #     for r in np.uint16(np.arange(filter))
-    #     return final_result
-
-
-    def feedforward(self):
-        """
-        Performs the feedforward action in the NN
-        :return:
-        """
-        self.layers = []
-        # create the first layer using the NN input x
-        layer = relu(np.dot(self.x, self.weights[0]))
-        self.layers.append(layer)
-
-        for i in range(len(self.weights)-1):
-            # for each weight in self.weights create a new layer based on the previous layer
-            layer = relu(np.dot(layer, self.weights[i+1]))
-            self.layers.append(layer)
-
-        # return the last (output) layer
-        return layer
-
-    def backprop(self):
-        """
-        Performs the back-propagation for the neural network
-        :return:
-        """
-        output_error = self.y - self.output
-
-        delta = output_error * relu_derivative(self.output)
-        # output_delta = np.dot(output_error, relu_derivative(self.output))
-        adjustments = [np.dot(self.layers[len(self.layers)-1].T, delta)]
-
-        # remove the output layer from our layers variable
-        self.layers.pop()
-
-        for index, layer in reversed(list(enumerate(self.layers))):
-            x = self.weights[index+1].T
-            error = np.dot(delta, self.weights[index+1].T)
-            # print("ERROR SHAPE: "+ str(error.shape))
-            # print("WEIGHTSHAPE: " + str(self.weights[index+1].shape))
-            delta = error * relu_derivative(layer)
-            if index is 0:
-                # this adds an extra element containing the input to the end of self.layers
-                # it allows the self.layers[index-1] access below to access the "-1" element which will be the last
-                # element in the list
-                self.layers.append(self.x)
-            adjustment = np.dot(self.layers[index-1].T, delta)
-            adjustments.insert(0, adjustment)
-
-        for index, adjustment in enumerate(adjustments):
-            # print("Adjustment: " + str(adjustment))
-            self.weights[index] += adjustment
-
-
-    def create_weights(self):
-        """
-        Create the weights for each layer in the NN. If there are 2 hidden layers, 3 sets of weights will be created
-        """
-        for i in range(self.n_layers-1):
-            # get the size of the start layer
-            in_size = self.layer_sizes[i]
-            # get the size of the next layer
-            out_size = self.layer_sizes[i+1]
-            # if len(self.hidden_layer_sizes) > i+1:
-            #     out_size = self.hidden_layer_sizes[i+1]
-            self.weights.append(np.random.uniform(-1/(math.sqrt(self.layer_sizes[i])),
-                                                  1/(math.sqrt(self.layer_sizes[i])),
-                                                  (in_size, out_size)))
+    #     for i in range(len(self.weights)-1):
+    #         # for each weight in self.weights create a new layer based on the previous layer
+    #         layer = relu(np.dot(layer, self.weights[i+1]))
+    #         self.layers.append(layer)
+    #
+    #     # return the last (output) layer
+    #     return layer
+    #
+    # def backprop(self):
+    #     """
+    #     Performs the back-propagation for the neural network
+    #     :return:
+    #     """
+    #     output_error = self.y - self.output
+    #
+    #     delta = output_error * relu_derivative(self.output)
+    #     # output_delta = np.dot(output_error, relu_derivative(self.output))
+    #     adjustments = [np.dot(self.layers[len(self.layers)-1].T, delta)]
+    #
+    #     # remove the output layer from our layers variable
+    #     self.layers.pop()
+    #
+    #     for index, layer in reversed(list(enumerate(self.layers))):
+    #         x = self.weights[index+1].T
+    #         error = np.dot(delta, self.weights[index+1].T)
+    #         # print("ERROR SHAPE: "+ str(error.shape))
+    #         # print("WEIGHTSHAPE: " + str(self.weights[index+1].shape))
+    #         delta = error * relu_derivative(layer)
+    #         if index is 0:
+    #             # this adds an extra element containing the input to the end of self.layers
+    #             # it allows the self.layers[index-1] access below to access the "-1" element which will be the last
+    #             # element in the list
+    #             self.layers.append(self.x)
+    #         adjustment = np.dot(self.layers[index-1].T, delta)
+    #         adjustments.insert(0, adjustment)
+    #
+    #     for index, adjustment in enumerate(adjustments):
+    #         # print("Adjustment: " + str(adjustment))
+    #         self.weights[index] += adjustment
+    #
+    #
+    # def create_weights(self):
+    #     """
+    #     Create the weights for each layer in the NN. If there are 2 hidden layers, 3 sets of weights will be created
+    #     """
+    #     for i in range(self.n_layers-1):
+    #         # get the size of the start layer
+    #         in_size = self.layer_sizes[i]
+    #         # get the size of the next layer
+    #         out_size = self.layer_sizes[i+1]
+    #         # if len(self.hidden_layer_sizes) > i+1:
+    #         #     out_size = self.hidden_layer_sizes[i+1]
+    #         self.weights.append(np.random.uniform(-1/(math.sqrt(self.layer_sizes[i])),
+    #                                               1/(math.sqrt(self.layer_sizes[i])),
+    #                                               (in_size, out_size)))
 
     # def train(self):
     #     self.output = self.feedforward()
@@ -254,7 +241,7 @@ def relu(x):
     Create the relu activation function. Decided to use the definitive relu
     :param x: numpy array of values
     """
-    return x * (x > 0)
+    return x * (x > 0) * 1.0
 
 
 def relu_derivative(x):
